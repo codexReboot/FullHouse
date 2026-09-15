726a614c990b7843912f42dd141245fe69e88673
Schalk van Dyk <schalkvandyk1979@gmail.com>
2026-09-15 14:43:18 +0200

    chore(project): configure source directory and dependencies
    * Track src directory
    * Ignore dist directory
    * Add dotenv dependency
    * Update package-lock.json

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import express from "express";
+import "dotenv/config";
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+// root page
+app.get("/", (req, res) => {
+	res.send("Hello from the FullHouse Project!");
+});
+
+app.listen(PORT, () => {
+	console.log(`Server running on PORT ${PORT}`);
+});
